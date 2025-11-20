cac17b468725c7bf7b0a90aa5f0ebd3c6426d06c
millipo14 <palina517@gmail.com>
2025-11-20 15:36:05 +0300

    Сборка проекта Ozon на Next.js

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import './scss/style.scss'
+import localFont from 'next/font/local'
+import Header from './ui/header';
+import Cart from './ui/Cart';
+
+const GTEestiProText = localFont({
+  src: [
+    {
+      path: './fonts/EestiRegular.otf',
+      weight: '500'
+    },
+    {
+      path: './fonts/EestiBold.otf',
+      weight: '700'
+    }
+  ],
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+      </head>
+      <body className={GTEestiProText.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Cart />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,50 @@
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-3 col-xl-2 d-none d-lg-block">
+            <div className="filter">
+              <div className="filter-title">
+                <h5>Фильтр</h5>
+              </div>
+              <div className="filter-price">
+                <div className="filter-price_title">
+                  Цена
+                </div>
+                <form>
+                  <div className="filter-price_range">
+                    <div className="filter-price_input-wrapper">
+                      <label htmlFor="min" className="filter-price_label">от</label>
+                      <input id="min" className="filter-price_input" />
+                    </div>
+                    <div className="filter-price_input-wrapper">
+                      <label htmlFor="max" className="filter-price_label">до</label>
+                      <input id="max" className="filter-price_input" />
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div className="filter-check">
+                <label className="filter-check_label">
+                  <input type="checkbox" className="filter-check_checkbox" id="discount-checkbox" />
+                  <span className="filter-check_checkmark"></span>
+                  <span className="filter-check_label-text">Акция</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-9 col-xl-10">
+            <div className="container">
+              <div className="row no-gutters goods">
+                {/* <!-- КАРТОЧКИ ТОВАРОВ -->
+                  <!-- END КАРТОЧКИ ТОВАРОВ --> */}
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
